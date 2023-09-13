edf2f01ec10bb29d5398ac9bf8891a6a4b606b5d
djdw04 <djdw04@student.cis.edu.hk>
2023-09-13 09:35:20 +0800

    Initial

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,19 @@
 import java.util.Scanner;
 public class Main {
-
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int age;
+        String firstName;
+        String favouriteFood;
+        System.out.println("Enter your age: ");
+        age = scanner.nextInt();
+        System.out.println("Enter your first name: ");
+        scanner.nextLine();
+        firstName = scanner.nextLine();
+        System.out.println("Enter your favourite food: ");
+        favouriteFood = scanner.nextLine();
+
+        System.out.println("First Name: " + firstName + "\nAge: " + age + "\nFavourite Food: " +favouriteFood);
 
 
 
